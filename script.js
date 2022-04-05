0e84babffca9bf9f6955c9f3671cde61cc4c7ec6
Luoxi Pan <l.panpax@gmail.com>
2022-04-05 20:59:03 +1000

    Add comment with pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Goal: Create 16 divs, each with 16 columns
+    Pseudocode:
+    let d = Create div element and add to parent to div
+    for 16 times, add parent div to d
+    Set d to flexbox (evenly spaced)
+*/
