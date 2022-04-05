c3ccd2989f6f896ab9f7402cc6032cebffc9c984
Luoxi Pan <l.panpax@gmail.com>
2022-04-05 21:12:15 +1000

    Create single row of div elements

--- script.js
+++ script.js
@@ -4,3 +4,21 @@
     for 16 times, add parent div to d
     Set d to flexbox (evenly spaced)
 */
+
+const COLUMNS = 16;
+
+gridContainer = document.querySelector('.gridContainer');
+
+rowDiv = document.createElement('div');
+rowDiv.classList.add('rowDiv');
+gridContainer.appendChild(rowDiv);
+
+console.log(rowDiv);
+
+for(let i = 0; i <= COLUMNS; i++) {
+    columnDiv = document.createElement('div');
+    columnDiv.textContent = 'C';
+    rowDiv.appendChild(columnDiv);
+}
+
+console.log(rowDiv);
